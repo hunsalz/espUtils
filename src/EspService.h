986e0da334cfde92c3c9e090dc11ccb9ed6db0b2
hunsalz <info@hunsalz.de>
2017-09-18 09:23:53 +0200

    added EspService

---
+++ src/EspService.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifdef ESP8266
+extern "C" {
+  #include "user_interface.h" // Expressif ESP8266 Non-OS API
+}
+#endif
+
+#include <ArduinoLog.h> // https://github.com/thijse/Arduino-Log
+#include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson
+
+#include "Service.h"
+
+class EspService : public Service {
+
+	public:
+
+    EspService();
+
+    ~EspService();
+
+    bool start();
+
+    bool stop();
+
+		JsonObject& getDetails();
+};
