301b2e9db7e311ec0feeb7ba03ee26d3264c2beb
hunsalz <info@hunsalz.de>
2017-12-27 10:45:00 +0100

    removed obsolete virtual isSetup method

--- src/actuator/Driver.h
+++ src/actuator/Driver.h
@@ -10,9 +10,6 @@ namespace esp8266util {
 
     public:
 
-      // use this method to know if this driver is setup or not
-      virtual bool isSetup() = 0;
-
       static uint16_t getPWMRange();
 
       static void setPWMRange(uint16_t pwmRange = 1023);
