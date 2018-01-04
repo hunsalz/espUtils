5c03af420e3788f422c1ccbe348fcfea5ee85f43
hunsalz <info@hunsalz.de>
2018-01-04 23:43:12 +0100

    added a single include option

---
+++ src/Esp8266Utils.h
@@ -0,0 +1,22 @@
+#ifndef ESP8266UTILS_H
+#define ESP8266UTILS_H
+
+#include <actuator/Driver.h>
+#include <actuator/MotorDriver.h>
+#include <network/MDNSService.h>
+#include <network/MQTTService.h>
+#include <network/WiFiAPService.h>
+#include <network/WiFiService.h>
+#include <sensor/BMP280Service.h>
+#include <sensor/DHTService.h>
+#include <sensor/MQ135Service.h>
+#include <Configurable.h>
+#include <EspService.h>
+#include <FSService.h>
+#include <JsonHelper.h>
+#include <NTPService.h>
+#include <Service.h>
+#include <WebService.h>
+#include <WebSocketListener.h>
+
+#endif // ESP8266UTILS_H
