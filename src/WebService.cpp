cffb18f337f16831a331b2914bda8de8e78cace5
hunsalz <info@hunsalz.de>
2018-02-13 13:50:15 +0100

    changed internal handling of AsyncWebserver

--- src/WebService.cpp
+++ src/WebService.cpp
@@ -2,74 +2,89 @@
 
 namespace esp8266util {
 
-WebService::~WebService() { getWebServer().reset(); }
+WebService::WebService(uint16_t port) : _webServer(port) {}
 
-bool WebService::available() { return _webServer; }
+bool WebService::begin() {
+  // add generic services registry resource
+  on("/services", HTTP_GET,
+     [this](AsyncWebServerRequest *request) { send(request, getServices()); });
+  // add default 404 handler
+  getWebServer().onNotFound([this](AsyncWebServerRequest *request) {
+    LOG.verbose(F("HTTP 404 : [http://%s%s] not found."),
+                request->host().c_str(), request->url().c_str());
+    // TODO make a nice 404 page
+    request->send(404, "text/plain", F("404 error - Page not found."));
+  });
 
-bool WebService::begin(uint16_t port) {
-  
-  if (!available()) {
-    _webServer = new AsyncWebServer(port);
-    // add generic services registry resource
-    on("/services", HTTP_GET, [this](AsyncWebServerRequest *request) { send(request, getDetails()); });
-    // add default 404 handler
-    getWebServer().onNotFound([this](AsyncWebServerRequest *request) {
-      LOG.verbose(F("HTTP 404 : [http://%s%s] not found."), request->host().c_str(), request->url().c_str());
-      // TODO make a nice 404 page
-      request->send(404, "text/plain", F("404 error - Page not found."));
-    });
+  // TODO SSL/TLS example:
+  // https://github.com/me-no-dev/ESPAsyncWebServer/issues/75 Works, but isn't
+  // reliable:
+  // https://github.com/esp8266/Arduino/issues/2733#issuecomment-264710234
+  // getWebServer().onSslFileRequest([](void * arg, const char *filename,
+  // uint8_t **buf) -> int {
+  //   File file = SPIFFS.open(filename, "r");
+  //   if (file) {
+  //     Serial.printf("SSL file found: %s\n", filename);
+  //     size_t size = file.size();
+  //     uint8_t * nbuf = (uint8_t*)malloc(size);
+  //     if (nbuf) {
+  //       size = file.read(nbuf, size);
+  //       file.close();
+  //       *buf = nbuf;
+  //       return size;
+  //     }
+  //     file.close();
+  //   } else {
+  //     Serial.printf("SSL file not found: %s\n", filename);
+  //   }
+  //   *buf = 0;
+  //   return 0;
+  // }, NULL);
+  // getWebServer().beginSecure("/server.cer", "/server.key", NULL);
 
-    // TODO SSL/TLS example: https://github.com/me-no-dev/ESPAsyncWebServer/issues/75
-    // Works, but isn't reliable: https://github.com/esp8266/Arduino/issues/2733#issuecomment-264710234
-    // getWebServer().onSslFileRequest([](void * arg, const char *filename, uint8_t **buf) -> int {
-    //   File file = SPIFFS.open(filename, "r");
-    //   if (file) {
-    //     Serial.printf("SSL file found: %s\n", filename);
-    //     size_t size = file.size();
-    //     uint8_t * nbuf = (uint8_t*)malloc(size);
-    //     if (nbuf) {
-    //       size = file.read(nbuf, size);
-    //       file.close();
-    //       *buf = nbuf;
-    //       return size;
-    //     }
-    //     file.close();
-    //   } else {
-    //     Serial.printf("SSL file not found: %s\n", filename);
-    //   }
-    //   *buf = 0;
-    //   return 0;
-    // }, NULL);
-    // getWebServer().beginSecure("/server.cer", "/server.key", NULL);
+  // start web server
+  _webServer.begin();
 
-    // start web server
-    getWebServer().begin();
+  LOG.verbose(F("WebServer started."));
 
-    LOG.verbose(F("WebServer started."));
-  }
-
-  return available();
+  return true;  // TODO
 }
 
-AsyncWebServer &WebService::getWebServer() { return *_webServer; }
-
-uint8_t WebService::getPort() { return _port; }
-
-AsyncCallbackWebHandler &WebService::on(const char *uri, WebRequestMethodComposite method, ArRequestHandlerFunction onRequest) {
-  
-  // (Caution) overwrites & disables "catchAllHandler" for onRequestBody(...) in ESPAsyncWebServer; maybe rework again
-  on(uri, method, onRequest, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {},
-     [](AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len, bool final) {});
+AsyncWebServer &WebService::getWebServer() {
+  // FIXME
+  return _webServer;
 }
 
-AsyncCallbackWebHandler &WebService::on(const char *uri, WebRequestMethodComposite method, ArRequestHandlerFunction onRequest, ArBodyHandlerFunction onBody) {
-  
+uint8_t WebService::getPort() {
+  return _port;
+}
+
+AsyncCallbackWebHandler &WebService::on(const char *uri,
+                                        WebRequestMethodComposite method,
+                                        ArRequestHandlerFunction onRequest) {
+  // (Caution) overwrites & disables "catchAllHandler" for onRequestBody(...) in
+  // ESPAsyncWebServer; maybe rework again
+  on(uri, method, onRequest,
+     [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index,
+        size_t total) {},
+     [](AsyncWebServerRequest *request, const String &filename, size_t index,
+        uint8_t *data, size_t len, bool final) {});
+}
+
+AsyncCallbackWebHandler &WebService::on(const char *uri,
+                                        WebRequestMethodComposite method,
+                                        ArRequestHandlerFunction onRequest,
+                                        ArBodyHandlerFunction onBody) {
   on(uri, method, onRequest, onBody,
-     [](AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len, bool final) {});
+     [](AsyncWebServerRequest *request, const String &filename, size_t index,
+        uint8_t *data, size_t len, bool final) {});
 }
 
-AsyncCallbackWebHandler &WebService::on(const char *uri, WebRequestMethodComposite method, ArRequestHandlerFunction onRequest, ArBodyHandlerFunction onBody, ArUploadHandlerFunction onUpload) {
-  
+AsyncCallbackWebHandler &WebService::on(const char *uri,
+                                        WebRequestMethodComposite method,
+                                        ArRequestHandlerFunction onRequest,
+                                        ArBodyHandlerFunction onBody,
+                                        ArUploadHandlerFunction onUpload) {
   // add uri to service listing
   _services.push_back(String(uri));
   // add to web server
@@ -77,37 +92,34 @@ AsyncCallbackWebHandler &WebService::on(const char *uri, WebRequestMethodComposi
 }
 
 void WebService::send(AsyncWebServerRequest *request, JsonObject &json) {
-  
   String payload = toString(json);
   LOG.verbose(F("Send response: %s."), payload.c_str());
   request->send(new AsyncBasicResponse(200, "text/json", payload));
 }
 
 void WebService::send(AsyncWebServerRequest *request, JsonArray &json) {
-  
   String payload = toString(json);
   LOG.verbose(F("Send response: %s."), payload.c_str());
   request->send(new AsyncBasicResponse(200, "text/json", payload));
 }
 
 void WebService::send(AsyncWebServerRequest *request, StreamString stream) {
-  
   request->send(new AsyncBasicResponse(200, "text/json", stream));
   LOG.verbose(F("Send response: %s."), stream.c_str());
 }
 
-JsonArray &WebService::getDetails() {
-  
+JsonArray &WebService::getServices() {
   DynamicJsonBuffer jsonBuffer;
   JsonArray &json = jsonBuffer.createArray();
-  for (std::vector<String>::iterator i = _services.begin(); i != _services.end(); ++i) {
+  for (std::vector<String>::iterator i = _services.begin();
+       i != _services.end(); ++i) {
     json.add(*i);
   }
 
   return json;
 }
-} // namespace esp8266util
+}  // namespace esp8266util
 
 #if !defined(NO_GLOBAL_INSTANCES)
-esp8266util::WebService SERVER;
-#endif // NO_GLOBAL_INSTANCES
+esp8266util::WebService SERVER = esp8266util::WebService(80);
+#endif  // NO_GLOBAL_INSTANCES
--- src/WebService.h
+++ src/WebService.h
@@ -1,34 +1,31 @@
 #ifndef WEB_SERVICE_H
 #define WEB_SERVICE_H
 
-#ifdef Arduino_h
-// Arduino is not compatible with std::vector
-#undef min
-#undef max
-#endif
-#include <vector>
+// #ifdef Arduino_h
+// // Arduino is not compatible with std::vector
+// #undef min
+// #undef max
+// #endif
+// #include <vector>
 
-#include <ESPAsyncWebServer.h> // https://github.com/me-no-dev/ESPAsyncWebServer
-#include <Log4Esp.h>           // https://github.com/hunsalz/log4Esp
-#include <StreamString.h>      // https://github.com/esp8266/Arduino/tree/master/cores/esp8266
+#include <ESPAsyncWebServer.h>  // https://github.com/me-no-dev/ESPAsyncWebServer
+#include <Log4Esp.h>            // https://github.com/hunsalz/log4Esp
+#include <StreamString.h>       // https://github.com/esp8266/Arduino/tree/master/cores/esp8266
 
-#include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson
+#include <ArduinoJson.h>  // https://github.com/bblanchon/ArduinoJson
 
 #include "JsonHelper.h"
-#include "WebSocketListener.h"
 
 using esp8266util::toString;
 
 namespace esp8266util {
 
 class WebService {
+ public:
 
-public:
-  ~WebService();
+  WebService(uint16_t port = 80);
 
-  bool available();
-
-  bool begin(uint16_t port = 80);
+  bool begin();
 
   // TODO bool end();
 
@@ -36,11 +33,17 @@ public:
 
   uint8_t getPort();
 
-  AsyncCallbackWebHandler &on(const char *uri, WebRequestMethodComposite method, ArRequestHandlerFunction onRequest);
+  AsyncCallbackWebHandler &on(const char *uri, WebRequestMethodComposite method,
+                              ArRequestHandlerFunction onRequest);
 
-  AsyncCallbackWebHandler &on(const char *uri, WebRequestMethodComposite method, ArRequestHandlerFunction onRequest, ArBodyHandlerFunction onBody);
+  AsyncCallbackWebHandler &on(const char *uri, WebRequestMethodComposite method,
+                              ArRequestHandlerFunction onRequest,
+                              ArBodyHandlerFunction onBody);
 
-  AsyncCallbackWebHandler &on(const char *uri, WebRequestMethodComposite method, ArRequestHandlerFunction onRequest, ArBodyHandlerFunction onBody, ArUploadHandlerFunction onUpload);
+  AsyncCallbackWebHandler &on(const char *uri, WebRequestMethodComposite method,
+                              ArRequestHandlerFunction onRequest,
+                              ArBodyHandlerFunction onBody,
+                              ArUploadHandlerFunction onUpload);
 
   void send(AsyncWebServerRequest *request, JsonObject &json);
 
@@ -48,19 +51,19 @@ public:
 
   void send(AsyncWebServerRequest *request, StreamString stream);
 
-  JsonArray &getDetails();
+  JsonArray &getServices();
 
-private:
-  AsyncWebServer *_webServer = NULL;
+ private:
+  AsyncWebServer _webServer;
 
   uint8_t _port = 80;
 
   std::vector<String> _services;
 };
-} // namespace esp8266util
+}  // namespace esp8266util
 
 #if !defined(NO_GLOBAL_INSTANCES)
 extern esp8266util::WebService SERVER;
-#endif // NO_GLOBAL_INSTANCES
+#endif  // NO_GLOBAL_INSTANCES
 
-#endif // WEB_SERVICE_H
+#endif  // WEB_SERVICE_H
