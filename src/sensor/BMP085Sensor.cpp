f8bea9a6de567cf6dd81aaabfbcf40c0b1f82cb4
hunsalz <info@hunsalz.de>
2018-08-14 16:27:51 +0200

    added BMP085 support

---
+++ src/sensor/BMP085Sensor.cpp
@@ -0,0 +1,70 @@
+#include "BMP085Sensor.h"
+
+namespace esp8266utils {
+
+bool BMP085Sensor::begin(float seaLevelPressure) {
+  _seaLevelPressure = seaLevelPressure;
+  _bmp085 = new Adafruit_BMP085_Unified();
+  return _bmp085->begin();
+}
+
+String BMP085Sensor::getConfigAsJson() {
+
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject &json = jsonBuffer.createObject();
+
+  return esp8266utils::toString(json);
+}
+
+Adafruit_BMP085_Unified &BMP085Sensor::getBMP085() {
+  return *_bmp085;
+}
+
+bool BMP085Sensor::update(bool mock) {
+  
+  bool update = false;
+  if (_bmp085 && !mock) {
+    sensors_event_t event;
+    _bmp085->getEvent(&event);
+    _pressure = event.pressure; // unit is Pascal (Pa) - // https://en.wikipedia.org/wiki/Pascal_(unit)
+    _bmp085->getTemperature(&_temperature); // unit is Celsius, °C
+    _altitude = _bmp085->pressureToAltitude(getSeaLevelPressure(), event.pressure); // use standard baseline - // https://en.wikipedia.org/wiki/Pressure_altitude
+    update = true;
+  } else {
+    _temperature = random(180, 310) / 10.0;
+    _pressure = random(10000, 12000);
+    _altitude = random(100, 120);
+    update = true;
+  }
+
+  return update;
+}
+
+float BMP085Sensor::getSeaLevelPressure() {
+  return _seaLevelPressure;
+}
+
+float BMP085Sensor::getTemperature() {
+  return _temperature;
+}
+
+float BMP085Sensor::getPressure() {
+  return _pressure;
+}
+
+float BMP085Sensor::getAltitude() {
+  return _altitude;
+}
+
+String BMP085Sensor::getValuesAsJson() {
+  
+  const size_t bufferSize = JSON_OBJECT_SIZE(3);
+  DynamicJsonBuffer jsonBuffer(bufferSize);
+  JsonObject &json = jsonBuffer.createObject();
+  json["temperature"] = getTemperature();
+  json["pressure"] = getPressure();
+  json["altitude"] = getAltitude();
+  
+  return esp8266utils::toString(json);
+}
+}  // namespace esp8266utils
---
+++ src/sensor/BMP085Sensor.h
@@ -0,0 +1,41 @@
+#ifndef BMP085_SENSOR_H
+#define BMP085_SENSOR_H
+
+#include <Adafruit_BMP085_U.h> // https://github.com/adafruit/Adafruit_BMP085_Unified
+
+#include "Configurable.h"
+
+namespace esp8266utils {
+
+class BMP085Sensor : public Configurable {
+
+public:
+  bool begin(float seaLevelPressure = 1013.25);
+
+  String getConfigAsJson();
+
+  Adafruit_BMP085_Unified &getBMP085();
+
+  bool update(bool mock = false);
+
+  float getSeaLevelPressure(); // unit is Pascal, Pa
+
+  float getTemperature(); // unit is Celsius, °C
+
+  float getPressure(); // unit is Pascal, Pa
+
+  float getAltitude(); // unit in Meter, m
+
+  String getValuesAsJson();
+
+private:
+  Adafruit_BMP085_Unified *_bmp085 = NULL;
+
+  float _seaLevelPressure = 1013.25;
+  float _temperature = NAN;
+  float _pressure = NAN;
+  float _altitude = NAN;
+};
+} // namespace esp8266utils
+
+#endif // BMP085_SENSOR_H
