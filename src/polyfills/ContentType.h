24affa76c607e3abc8e1a0c00f7316f55c1eec1b
hunsalz <info@hunsalz.de>
2019-01-10 16:47:20 +0100

    changed some const names

--- src/polyfills/ContentType.h
+++ src/polyfills/ContentType.h
@@ -6,15 +6,15 @@ const char* TEXT_CSS = "text/css";
 const char* TEXT_HTML = "text/html";
 const char* TEXT_PLAIN = "text/plain";
 const char* TEXT_XML = "text/xml";
-const char* APP_JS = "application/javascript";
-const char* APP_JSON = "application/json";
-const char* APP_PDF = "application/pdf";
-const char* APP_ZIP = "application/zip";
-const char* APP_GZ = "application/x-gzip";
-const char* IMG_GIF = "image/gif";
-const char* IMG_ICO = "image/x-icon";
-const char* IMG_JPG = "image/jpeg";
-const char* IMG_PNG = "image/png";
-const char* IMG_SVG = "image/svg+xml";
+const char* APPLICATION_JS = "application/javascript";
+const char* APPLICATION_JSON = "application/json";
+const char* APPLICATION_PDF = "application/pdf";
+const char* APPLICATION_ZIP = "application/zip";
+const char* APPLICATION_GZ = "application/x-gzip";
+const char* IMAGE_GIF = "image/gif";
+const char* IMAGE_ICO = "image/x-icon";
+const char* IMAGE_JPG = "image/jpeg";
+const char* IMAGE_PNG = "image/png";
+const char* IMAGE_SVG = "image/svg+xml";
 
-} // namespace esp8266utils
+}  // namespace esp8266utils
