31930659cb490234c0717504982a486e414e4bd4
hunsalz <info@hunsalz.de>
2019-01-25 15:38:12 +0100

    added getDevice(char* buffer)

--- src/sensor/Sensor.hpp
+++ src/sensor/Sensor.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
-#include <Arduino.h>  // https://github.com/esp8266/Arduino/blob/master/cores/esp8266/Arduino.h
-#include <ESP.h>  // https://github.com/esp8266/Arduino/blob/master/cores/esp8266/Esp.h
+#include <ESP.h>  // https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/Esp.h
+                  // https://github.com/esp8266/Arduino/blob/master/cores/esp8266/Esp.h
 
-namespace ESPUtils {
+namespace espUtils {
 
 class Sensor {
 
+ public:
+
+  int getDevice(char* buffer) {
+  
+    #ifdef ESP32
+      return sprintf(buffer, "ESP-%010x", (uint32_t)ESP.getEfuseMac()); // use low 4 bytes
+    #else
+      return sprintf(buffer, "ESP-%010x", ESP.getChipId());
+    #endif
+  }
 };
-}  // namespace ESPUtils
+}  // namespace espUtils
